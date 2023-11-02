b898f0bf101fdddccc717ffb39d9c4daa6b9365a
Julian Rüth <julian.rueth@fsfe.org>
2023-11-02 22:19:11 +0200

    Do not use C++17 to compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@
 export CFLAGS="-g -O3 $CFLAGS"
 export CXXFLAGS="-g -O3 $CXXFLAGS"
 
+# eclib uses std::ptr_fun which was removed in C++17.
+export CXXFLAGS="$CXXFLAGS -std=c++14"
+
 # workaround for https://github.com/JohnCremona/eclib/issues/45
 if [ "$(uname)" == "Linux" ]; then
     sed -i "s/long    rank(GEN x);//g" $PREFIX/include/pari/paridecl.h
